36ed85f3cc6be3acea553894965293277840fea9
Viktoria_Khoruzheva <vitkoria_smirnova_1996@mail.ru>
2021-12-06 12:54:41 +0300

    Домашнее задание 1

---
+++ src/main/java/Application.java
@@ -0,0 +1,28 @@
+package main.java;
+
+import main.java.animals.Kotik;
+
+public class Application {
+    public static void main(String[] args) {
+        Kotik kotik1 = new Kotik("Пушок", "мяу", 8, 3000);
+        Kotik kotik2 = new Kotik();
+        kotik2.setName("Орешек");
+        kotik2.setVoice("мяу");
+        kotik2.setSatiety(0);
+        kotik2.setWeight(5350);
+        String[] actionsKotik1 = kotik1.liveAnotherDay();
+        for (String action : actionsKotik1) {
+            System.out.println(action);
+        }
+        System.out.println("имя: " + kotik2.getName() + ", вес: " + kotik2.getWeight());
+        System.out.println(compareVoice(kotik1, kotik2));
+        System.out.println(Kotik.getCount());
+    }
+
+    public static boolean compareVoice(Kotik kotik1, Kotik kotik2) {
+        if (kotik1.getVoice() == null) {
+            return false;
+        }
+        return kotik1.getVoice().equals(kotik2.getVoice());
+    }
+}
---
+++ src/main/java/animals/Kotik.java
@@ -0,0 +1,172 @@
+package main.java.animals;
+
+public class Kotik {
+    private String name;
+    private String voice;
+    private int satiety;
+    private int weight;
+    private static int count;
+    private final int METHODS = 5;
+
+    public Kotik(String name, String voice, int satiety, int weight) {
+        this.name = name;
+        this.voice = voice;
+        this.satiety = satiety;
+        this.weight = weight;
+        Kotik.count++;
+    }
+
+    public Kotik() {
+        Kotik.count++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getVoice() {
+        return voice;
+    }
+
+    public int getSatiety() {
+        return satiety;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public static int getCount() {
+
+        return count;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVoice(String voice) {
+        this.voice = voice;
+    }
+
+    public void setSatiety(int satiety) {
+        this.satiety = satiety;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public static void setCount(int count) {
+        Kotik.count = count;
+    }
+
+    boolean play() {
+        if (satiety > 0) {
+            satiety--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean sleep() {
+        if (satiety > 0) {
+            satiety--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean wash() {
+        if (satiety > 0) {
+            satiety--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean walk() {
+        if (satiety > 0) {
+            satiety--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean hunt() {
+        if (satiety > 0) {
+            satiety--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void eat(int satiety) {
+        this.satiety = this.satiety + satiety;
+    }
+
+    void eat(int satiety, String food) {
+        eat(satiety);
+    }
+
+    void eat() {
+        eat(4, "корм");
+    }
+
+    public String[] liveAnotherDay() {
+        String[] actions = new String[24];
+        for (int i = 0; i < 24; i++) {
+            int random = (int) (Math.random() * METHODS) + 1;
+            switch (random) {
+                case 1:
+                    if (play()) {
+                        actions[i] = i + " - играл";
+                    } else {
+                        actions[i] = i + " - ел";
+                        eat();
+                    }
+                    break;
+                case 2:
+                    if (sleep()) {
+                        actions[i] = i + " - спал";
+                    } else {
+                        actions[i] = i + " - ел";
+                        eat();
+                    }
+                    break;
+                case 3:
+                    if (wash()) {
+                        actions[i] = i + " - умывался";
+                    } else {
+                        actions[i] = i + " - ел";
+                        eat();
+                    }
+                    break;
+                case 4:
+                    if (walk()) {
+                        actions[i] = i + " - гулял";
+                    } else {
+                        actions[i] = i + " - ел";
+                        eat();
+                    }
+                    break;
+                case 5:
+                    if (hunt()) {
+                        actions[i] = i + " - охотился";
+                    } else {
+                        actions[i] = i + " - ел";
+                        eat();
+                    }
+                    break;
+
+            }
+        }
+        return actions;
+    }
+
+}
